a10d05f4280d9c06507a4257416973754b54c4bb
Lukas Witzigmann <lukas.witzigmann2@gmail.com>
2024-12-14 12:43:20 +0100

    FPW-7: Fix floating point inaccuracies

--- src/app/services/svg-arrow.service.ts
+++ src/app/services/svg-arrow.service.ts
@@ -33,6 +33,7 @@ export class SvgArrowService {
         if (to instanceof SVGCircleElement) {
             return this.calculateCircleIntersection(x1, y1, x2, y2, to);
         } else if (to instanceof SVGGElement) {
+            //TODO cx and cy do not exist on the g element. Maybe try to find the rect within this g?
             const rectX = parseFloat(to.getAttribute('cx') || '0');
             const rectY = parseFloat(to.getAttribute('cy') || '0');
             const rectWidth = parseFloat(to.getAttribute('width') || '0');
@@ -78,14 +79,19 @@ export class SvgArrowService {
     }
 
     private calculateLineIntersection(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, x4: number, y4: number) {
+        // epsilon for floating point inaccuracies
+        // TODO Can we reuse this method for cut detection?
+        const epsilon = 1e-10;
         const denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-        if (denom === 0) return null;
+        if (Math.abs(denom) < epsilon) return null;
 
         const intersectX = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denom;
         const intersectY = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denom;
 
-        if (intersectX < Math.min(x1, x2) || intersectX > Math.max(x1, x2) || intersectX < Math.min(x3, x4) || intersectX > Math.max(x3, x4)) return null;
-        if (intersectY < Math.min(y1, y2) || intersectY > Math.max(y1, y2) || intersectY < Math.min(y3, y4) || intersectY > Math.max(y3, y4)) return null;
+        if (intersectX < Math.min(x1, x2) - epsilon || intersectX > Math.max(x1, x2) + epsilon ||
+            intersectX < Math.min(x3, x4) - epsilon || intersectX > Math.max(x3, x4) + epsilon) return null;
+        if (intersectY < Math.min(y1, y2) - epsilon || intersectY > Math.max(y1, y2) + epsilon ||
+            intersectY < Math.min(y3, y4) - epsilon || intersectY > Math.max(y3, y4) + epsilon) return null;
 
         return {x: intersectX, y: intersectY};
     }
