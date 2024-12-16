8984417dfb808bea32de0ce08783738383272c51
TilDans <htsanders@web.de>
2024-12-16 16:14:46 +0100

    FPW-7, FPW-22 Grundlagen für Petrinetz Layout. Datenstruktur im Netz sowie Methoden zum Hinzufügen und zum Löschen implementiert.

---
+++ src/app/classes/Datastructure/InductiveGraph/PetriLayout/petriLayerContainer.ts
@@ -0,0 +1,74 @@
+import { CustomElement } from "../Elements/element";
+import { PetriLayer } from "./petriLayer";
+
+
+export class PetriLayerContainer extends Array<PetriLayer> {
+    
+    // Layer bis zum genannten item nach hinten schieben sowie neues mit dem Element einfügen davor
+    public insertToNewLayerBeforeCurrentElement(formerElement: CustomElement, replacingElement: CustomElement, newElement: CustomElement) {
+        const layerIndex = this.findIndex(petriLayer => petriLayer.includes(formerElement));
+        if (layerIndex == -1) {
+            console.log(`Something went wrong, element not contained within the layers`);
+        } else {
+            //Elemente von hinten an nach rechts schieben, jeweils auf index + 1. 
+            for (let i = this.length - 1; i >= layerIndex; i --) {
+                this [i + 1] = this [i];
+            }
+            //vorheriges Element durch neues ersetzen
+            this[layerIndex + 1][this[layerIndex + 1].indexOf(formerElement)] = replacingElement;
+            //weiteres Element in neu erzeugtes Layer vor dem bisherigen einfügen
+            this[layerIndex] = new PetriLayer(newElement);
+        }
+    }
+
+    // Layer ab dem genannten item nach hinten schieben.
+    public insertToNewLayerAfterCurrentElement(formerElement: CustomElement, replacingElement: CustomElement, newElement: CustomElement) {
+        const layerIndex = this.findIndex(petriLayer => petriLayer.includes(formerElement));
+        if (layerIndex == -1) {
+            console.log(`Something went wrong, element not contained within the layers`);
+        } else if (layerIndex == this.length - 1) { //letztes Layer im Petrinetz
+            //vorheriges Element durch neues ersetzen
+            this[layerIndex][this[layerIndex].indexOf(formerElement)] = replacingElement;
+            //neues Layer am Ende hinzufügen
+            this.push(new PetriLayer(newElement));
+        } else {
+            //Elemente von hinten an nach rechts schieben, jeweils auf index + 1. Zuletzt wird layerIndex + 2 geschrieben.
+            for (let i = this.length - 1; i > layerIndex; i --) {
+                this [i + 1] = this [i];
+            }
+            //vorheriges Element durch neues ersetzen
+            this[layerIndex][this[layerIndex].indexOf(formerElement)] = replacingElement;
+            //weiteres Element in neu erzeugtes Layer einfügen
+            this[layerIndex + 1] = new PetriLayer(newElement);
+        }
+    }
+
+    // Layer ab dem genannten item nach hinten schieben.
+    public insertToExistingLayerAfterCurrentElement(formerElement: CustomElement, replacingElement: CustomElement, newElement: CustomElement) {
+        const layerIndex = this.findIndex(petriLayer => petriLayer.includes(formerElement));
+        if (layerIndex == -1) { //Element nicht vorhanden
+            console.log(`Something went wrong, element not contained within the layers`);
+        } else {
+            //Index des gerade zu bearbeitenden Layers
+            const currentLayer = this[layerIndex];
+            //Index des zu bearbeitenden Elements 
+            const indexInLayer = currentLayer.indexOf(formerElement);
+            if (indexInLayer == currentLayer.length - 1) { //Element befindet sich am Ende der Liste
+                //vorheriges Element durch neues ersetzen
+                currentLayer[indexInLayer] = replacingElement;
+                //weiteres Element einfügen
+                currentLayer.push(newElement);
+            } else {
+                //Elemente nach rechts schieben, letztes Element ist indexInLayer + 2.
+                for (let i = currentLayer.length - 1; i > indexInLayer; i --) {
+                    this [i + 1] = this [i];
+                }
+                //vorheriges Element durch neues ersetzen
+                currentLayer[indexInLayer] = replacingElement;
+                //weiteres Element in bestehendes Layer einfügen
+                currentLayer[indexInLayer + 1] = newElement;
+            }
+        }
+    }
+
+}
