a2e0f7448d6c76f4f90a5ed099fe13cbbd1ea7a4
TilDans <htsanders@web.de>
2024-12-19 09:37:01 +0100

    Variante gelöscht

--- src/app/classes/Datastructure/InductiveGraph/Elements/eventLogDFG.ts
+++ src/app/classes/Datastructure/InductiveGraph/Elements/eventLogDFG.ts
@@ -31,24 +31,9 @@ export class EventLogDFG extends CustomElement{
         this.x = xNew;
         this.y = yNew;
 
-        //Variante A: Ausnutzen von transform translate bzgl der Gruppe
         this._svgElement!.setAttribute('transform', 'translate(' + xNew + ',' + yNew + ')');
         this._svgElement!.setAttribute('cx', xNew.toString());
         this._svgElement!.setAttribute('cy', yNew.toString());
-
-
-        //Variante B: OffSet der einzelnen Elemente und des Rectangles einzeln
-        /* const container = Array.from(this._svgElement!.children!);
-        container.forEach(element => {
-            console.log(xNew, yNew);
-            //x und y koordinaten der enthaltenen Elemente anpassen
-            const xInRectangle = parseFloat(element.getAttribute('cx') || '0');
-            const yInRectangle = parseFloat(element.getAttribute('cy') || '0');
-            element.setAttribute('cx', (xInRectangle + xNew).toString());
-            element.setAttribute('cy', (yInRectangle + yNew).toString());
-        });
-        const rects = this._svgElement!.getElementsByTagName('rect');
-        rects[0].setAttribute('transform', 'translate(' + xNew + ',' + yNew + ')'); */
     }
     
     public override getCenterXY(): { x: number; y: number; } {
