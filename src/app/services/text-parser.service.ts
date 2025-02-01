4cb92bd61288ff2608d5dca26bbcd7e3977a3b31
TilDans <htsanders@web.de>
2025-02-01 21:03:11 +0100

    fix generation of an empty trace in case of an abstract
    example:
    a b+ \n
    generated an empty trace with "" as element

--- src/app/services/text-parser.service.ts
+++ src/app/services/text-parser.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {EventLog} from '../classes/Datastructure/event-log/event-log';
 import {Trace} from '../classes/Datastructure/event-log/trace';
 import {TraceEvent} from '../classes/Datastructure/event-log/trace-event';
+import { sequence } from '@angular/animations';
 
 @Injectable({
     providedIn: 'root'
@@ -14,16 +15,12 @@ export class TextParserService {
     parse(eventLog: string): EventLog {
         // remove all duplicate spaces
         const sequences = eventLog.split('+');
-        console.log(sequences);
+        eventLog = eventLog.replace(/\s+/g, ' ');
 
-        const traces = sequences.filter(element => element.length > 0).map(sequence => {
-            eventLog = eventLog.replace(/\s+/g, ' ');
-            const events = sequence.split(' ')
-                .filter(el => el.length > 0)
-                .map(eventName => new TraceEvent(eventName.trim()));
+        const traces = sequences.filter(sequence => sequence.length > 0).map(trace => {
+            const events = trace.split(' ').filter(trace => trace.length > 0).map(eventName => new TraceEvent(eventName.trim()));
             return new Trace(events);
         });
-        console.log(traces);
-        return new EventLog(traces);
+        return new EventLog(traces.filter(trace => trace.events[0].conceptName != ""));
     }
 }
