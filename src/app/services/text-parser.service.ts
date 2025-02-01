9b5f0c5c5685cf0df89c83b67029cc74523d2459
TilDans <htsanders@web.de>
2025-02-01 19:11:06 +0100

    bugfix text parser

--- src/app/services/text-parser.service.ts
+++ src/app/services/text-parser.service.ts
@@ -13,14 +13,17 @@ export class TextParserService {
 
     parse(eventLog: string): EventLog {
         // remove all duplicate spaces
-        eventLog = eventLog.replace(/\s+/g, ' ');
         const sequences = eventLog.split('+');
+        console.log(sequences);
+
         const traces = sequences.filter(element => element.length > 0).map(sequence => {
+            eventLog = eventLog.replace(/\s+/g, ' ');
             const events = sequence.split(' ')
                 .filter(el => el.length > 0)
                 .map(eventName => new TraceEvent(eventName.trim()));
             return new Trace(events);
         });
+        console.log(traces);
         return new EventLog(traces);
     }
 }
